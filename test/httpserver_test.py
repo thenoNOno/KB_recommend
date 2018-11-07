a2a48dbc4802ba6346fbc3d6d546a404f806f7fc
thenoNOno <nowitherwillow@gmail.com>
2018-11-07 18:38:51 +0800

    fixbug

--- test/httpserver_test.py
+++ test/httpserver_test.py
@@ -8,10 +8,11 @@ def main():
 
     """
     #content_url = 'http://localhost:9092/api/KB_recommend/assess_content'
-    content_url = 'http://192.168.100.42:9090/api/KB_recommend/assess_content'
+    content_url = 'http://192.168.100.42:9092/api/KB_recommend/assess_content'
     data = {'pid':'123', 'content':'testcontent'}
     content = requests.post(content_url, data)
     print(content.content)
 
 if __name__ == "__main__":
     main()
+main()
